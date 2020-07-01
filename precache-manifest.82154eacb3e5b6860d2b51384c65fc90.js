1238fec4c15ff362527f13118cfea6f2b3f34858
Sriya <tsriya6@gmail.com>
2020-07-01 15:34:37 +0530

    Updates

---
+++ precache-manifest.82154eacb3e5b6860d2b51384c65fc90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5faa17ff520884f34b62f141597cc640",
+    "url": "/Task-Manager-React-App/index.html"
+  },
+  {
+    "revision": "c7b04f6cd4ac2dd75514",
+    "url": "/Task-Manager-React-App/static/css/main.aaa9e1dd.chunk.css"
+  },
+  {
+    "revision": "83cdb520d9c2aad89b77",
+    "url": "/Task-Manager-React-App/static/js/2.3a2d6b69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task-Manager-React-App/static/js/2.3a2d6b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b04f6cd4ac2dd75514",
+    "url": "/Task-Manager-React-App/static/js/main.97afa901.chunk.js"
+  },
+  {
+    "revision": "15015b6c5121c9d61545",
+    "url": "/Task-Manager-React-App/static/js/runtime-main.ac0ba354.js"
+  }
+]);
